8f23c59cc70bd695e328cf43a84011050067b8e0
zunema <yxxa.abc@gmail.com>
2020-11-10 08:21:06 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e506e87409c96d04dcdbbb7273790ca06446ef4e9f00d32c929f8e7fe35a58c009b484b9b08badd81fcb66dcec45ee5fa1ef5fb2e35a473cf540ff0a892e1a9a'
